e7105460dd6323b66a9a20050a0117b12b372fa7
Shubham Saxena <117487370+shubz23@users.noreply.github.com>
2024-11-15 02:00:15 +0530

    Update Homepage.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -35,7 +35,7 @@ function Homepage() {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans">
-          <b>Text-Karo</b>
+          <b>Text-Kro</b>
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
